f96d7db5c8cd7d59a564daf94a22ba87dfd4c704
hateman31 <metilogrunge31@gmail.com>
2024-04-06 15:42:27 +0300

    csv generating

---
+++ app/utils.py
@@ -0,0 +1,34 @@
+
+def rows_to_csv(rows,filename, headers=None):
+    with open(f'{filename}.csv', 'w') as csv:
+        if headers:
+            for header in headers:
+                csv.write(f"{header}\t")
+            csv.write('\n')
+        for row in rows:
+            for value in row:
+                csv.write(f"{value}\t")
+            csv.write('\n')
+
+    return True
+
+
+if __name__ == '__main__':
+    from db_actions import SQL
+    from db_config import CONN_STR
+    from pathlib import Path
+
+    db_client = SQL(CONN_STR)
+
+    fname = Path.cwd() / 'test'
+    rows = db_client.get_catalog()
+    headers = 'product_id product_name price category'.split(' ')
+
+    # print(rows)
+
+    rows_to_csv(rows, fname, headers=headers)
+
+    if fname.exists():
+        print('CSV file successfully created!')
+    else:
+        print('CSV file creating failured!')
